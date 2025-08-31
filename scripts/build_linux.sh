e7495cf28bb8f8ecaa4b794e81aeb9ca89d6e044
Matthew Ballance <matt.ballance@gmail.com>
2025-08-31 20:04:55 +0000

    Fix CI
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -5,7 +5,7 @@ yum install -y glibc-static
 yum install -y java-11-openjdk-devel uuid-devel libuuid-devel
 
 echo "BUILD_NUM=${BUILD_NUM}" >> python/zsp_fe_parser/__build_num__.py
-${IVPM_PYTHON} -m pip install ivpm cython setuptools --pre
+${IVPM_PYTHON} -m pip install ivpm cython setuptools
 ${IVPM_PYTHON} -m ivpm update -a --py-prerls-packages
 
 PYTHON=./packages/python/bin/python
